eb299abc6ea85379029b54b2df06f6bf5455e93a
Wenfeng CAI <caiwenfeng@suda.edu.cn>
2016-08-29 18:12:42 +0800

    Add deploy.sh for github pages

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -d $1 ] ; then
+    if [ ! -e deploy/$1 ] ; then
+        mkdir deploy/$1
+    fi
+    pandoc --template template.html -s -i -t revealjs -p \
+        --slide-level=2 --highlight-style=espresso \
+        -V theme=black -V history \
+        -V revealjs-url=//cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0 \
+        $1/index.md -o deploy/$1/index.html
+    if [ -d $1/res ] ; then
+        rsync -a --delete $1/res deploy/$1
+    fi
+    rsync -a --delete deploy/$1 x@10.10.12.160:/home/x/projects/coursesite/static/cw
+fi
